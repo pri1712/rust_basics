e7b4f77c90fef3bf00c61698820909caa35bb314
Priyanshu Soni <priyanshu171203@gmail.com>
2026-01-31 05:46:54 +0530

    channels

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,14 +1,39 @@
 use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
 
 fn main() {
-
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vec = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("clone"),
+            String::from("thread"),
+        ];
+        for val in vec {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(2));
         }
     });
-    thread::spawn(||{
-        println!("Hello world part 2");
+
+    thread::spawn(move || {
+        let vec = vec![
+            String::from("val1"),
+            String::from("val2"),
+            String::from("val3"),
+            String::from("val4"),
+        ];
+        for val in vec {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
-    handle.join().unwrap();
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
 }
