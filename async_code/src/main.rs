46bb0d32f07aa463b23f70b167db1b3594e9b8e5
Priyanshu Soni <priyanshu171203@gmail.com>
2026-02-09 06:41:51 +0530

    basics of async programming in rust

--- async_code/src/main.rs
+++ async_code/src/main.rs
@@ -1,4 +1,5 @@
 use std::env::args;
+use std::sync::mpsc;
 use std::time::Duration;
 use trpl::{block_on, Either, Html};
 
@@ -30,22 +31,62 @@ fn main() {
     //     }
     // })
 
-    trpl::block_on(async {
-        let run_one = async {
-            for i in 1..10  {
-                println!("Printing {} from run_one",i);
+    // trpl::block_on(async {
+    //     let run_one = async {
+    //         for i in 1..10  {
+    //             println!("Printing {} from run_one",i);
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+    //
+    //     let handle_one = run_one.await;
+    //
+    //     let run_two = async {
+    //         for i in 1..10  {
+    //             println!("Printing {} from run_two",i);
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+    //     let handle_two = run_two.await;
+    // })
+    trpl::block_on( async {
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
         };
 
-        let handle_one = run_one.await;
-
-        let run_two = async {
-            for i in 1..10  {
-                println!("Printing {} from run_two",i);
-                trpl::sleep(Duration::from_millis(500)).await;
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
             }
         };
-        let handle_two = run_two.await;
-    })
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join!(tx1_fut, tx_fut, rx_fut);
+    });
 }
